768ae11dadcc8cc0c480a917a236ada87a1d354c
Vinicius Picolo <picolo.vininicius@gmail.com>
2024-01-23 16:26:22 -0300

    Manipulando CSS pelo id atraves do JavaScript

---
+++ Dominando e Manipulando o DOM/Manipulando CSS Atraves do id/js/script.js
@@ -0,0 +1,7 @@
+var element = document.getElementById('box');
+
+element.style.width = "150px";
+element.style.height = "150px";
+element.style.backgroundColor = "green";
+element.style.marginLeft = "200px";
+element.style.marginTop = "200px";
