0c37194e2714f712d4fafc2cbfb76c02e291a1f1
Vinicius Picolo <picolo.vininicius@gmail.com>
2024-01-24 09:06:40 -0300

    Aprendendo a utilizacao do querySelectorAll

---
+++ Dominando e Manipulando o DOM/Dominando querySelectorAll/js/script.js
@@ -0,0 +1,9 @@
+var elements_1 = document.querySelectorAll(".testes .inside-testes");
+
+elements_1[0].style.fontSize = "50px";
+elements_1[0].style.color = "red";
+
+var elements_2 = document.querySelectorAll(".testes");
+
+elements_2[1].style.fontSize = "50px";
+elements_2[1].style.color = "blue";
